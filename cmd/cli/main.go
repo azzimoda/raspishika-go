7a103d51bb0957b3fb421f5c29e71781be207b42
Konstantin Matsa <konstantinmaca@gmail.com>
2025-10-31 23:09:22 +0500

    feat(cli): Add flag `--start` for scheduled start.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"time"
+
 	"github.com/azzimoda/raspishika-go/internal/app"
 	"github.com/azzimoda/raspishika-go/internal/config"
 	"github.com/azzimoda/raspishika-go/pkg/logger"
@@ -10,7 +13,13 @@ import (
 
 const ConfigFile = `configs/config.yml`
 
+var scheduledStartTime = flag.String("start", "", "time for scheduled start in format 2006-01-02T15:04")
+
 func main() {
+	flag.Parse()
+
+	log.Trace().Msgf("Scheduled start time: %v", *scheduledStartTime)
+
 	cfg, err := config.Load(ConfigFile)
 	if err != nil {
 		log.Panic().Err(err).Msg("Failed to load configuration")
@@ -25,6 +34,21 @@ func main() {
 	app, err := app.New(cfg)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to create application")
+	}
+
+	if scheduledStartTime != nil && *scheduledStartTime != "" {
+		startTime, err := time.Parse("2006-01-02T15:04", *scheduledStartTime)
+		startTime = time.Date(
+			startTime.Year(), startTime.Month(), startTime.Day(),
+			startTime.Hour(), startTime.Minute(), 0, 0,
+			time.Now().Location(),
+		)
+
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to parse start time")
+		}
+		log.Info().Msgf("Scheduled start time: %s", startTime.String())
+		time.Sleep(time.Until(startTime))
 	}
 
 	if err := app.Run(); err != nil {
