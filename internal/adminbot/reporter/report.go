38195de247a639552d02bacd00eacde9797910f7
mazza <konstantinmaca@gmail.com>
2026-01-28 22:21:00 +0500

    feat(adminbot/report): Add temp flag and improve logging.
    refactor(adminbot/report): Refactor code generally.

--- internal/adminbot/reporter/report.go
+++ internal/adminbot/reporter/report.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"runtime"
+	"time"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -14,8 +15,11 @@ import (
 	"github.com/azzimoda/raspishika-go/pkg/utils"
 )
 
+type reportKey string
+
 const (
 	logKey      reportKey = "log"
+	tempKey     reportKey = "temp"
 	errKey      reportKey = "err"
 	chatIDKey   reportKey = "chat_id"
 	usernameKey reportKey = "username"
@@ -27,13 +31,14 @@ type Reporter interface {
 }
 
 func NewReportConfig(bot *bot.Bot, recipientChatID int64) ReportConfig {
-	return ReportConfig{bot: bot, recipientChatID: recipientChatID, ctx: defaultContext()}
+	return ReportConfig{bot: bot, recipientChatID: recipientChatID, Context: defaultContext()}
 }
 
 func defaultContext() context.Context {
 	ctx := context.Background()
 
 	ctx = context.WithValue(ctx, logKey, false)
+	ctx = context.WithValue(ctx, tempKey, false)
 	ctx = context.WithValue(ctx, errKey, nil)
 	ctx = context.WithValue(ctx, chatIDKey, int64(0))
 	ctx = context.WithValue(ctx, usernameKey, "")
@@ -43,19 +48,24 @@ func defaultContext() context.Context {
 }
 
 type ReportConfig struct {
+	context.Context
 	bot             *bot.Bot
 	recipientChatID int64
-	ctx             context.Context
 }
 
-type reportKey string
-
+// Log makes the report be printed as log.
 func (r ReportConfig) Log() ReportConfig {
 	return r.withValue(logKey, true)
 }
 
+// Err adds error to report message.
 func (r ReportConfig) Err(err error) ReportConfig {
 	return r.withValue(errKey, err)
+}
+
+// Temp makes the report message be deleted after a minute.
+func (r ReportConfig) Temp() ReportConfig {
+	return r.withValue(tempKey, true)
 }
 
 // Chat sets the chat, whose message caused the error. It can be either a Chat object or a chat ID.
@@ -72,11 +82,11 @@ func (r ReportConfig) Chat(chatOrID any) ReportConfig {
 
 // Debug sets a debug object with the given name and value.
 func (r ReportConfig) Debug(name string, value any) ReportConfig {
-	debugValues, ok := r.ctx.Value(debugKey).(map[string]any)
+	debugValues, ok := r.Value(debugKey).(map[string]any)
 	if !ok {
 		debugValues = make(map[string]any)
 		debugValues[name] = value
-		r.ctx = context.WithValue(r.ctx, debugKey, debugValues)
+		r.Context = context.WithValue(r.Context, debugKey, debugValues)
 	} else {
 		debugValues[name] = value
 	}
@@ -84,7 +94,7 @@ func (r ReportConfig) Debug(name string, value any) ReportConfig {
 }
 
 func (r ReportConfig) withValue(key any, value any) ReportConfig {
-	r.ctx = context.WithValue(r.ctx, key, value)
+	r.Context = context.WithValue(r.Context, key, value)
 	return r
 }
 
@@ -96,21 +106,26 @@ func (rc ReportConfig) Msg(text string) (*models.Message, error) {
 		caller = fmt.Sprintf("%s:%d", file, line)
 	}
 
-	doLog := rc.ctx.Value(logKey).(bool)
-	reportErr, ok := rc.ctx.Value(errKey).(error)
+	doLog := rc.Value(logKey).(bool)
+	isTemp := rc.Value(tempKey).(bool)
+	reportErr, ok := rc.Value(errKey).(error)
 	if !ok {
 		reportErr = nil
 	}
-	chatID := rc.ctx.Value(chatIDKey).(int64)
-	username := rc.ctx.Value(usernameKey).(string)
-	debugObjects := rc.ctx.Value(debugKey).(map[string]any)
+	chatID := rc.Value(chatIDKey).(int64)
+	username := rc.Value(usernameKey).(string)
+	debugObjects := rc.Value(debugKey).(map[string]any)
 
-	if doLog {
+	// NOTE: Log the report when it is temporary to not lose information.
+	if doLog || isTemp {
 		var logEvent *zerolog.Event
 		if reportErr != nil {
 			logEvent = log.Error().Err(reportErr)
 		} else {
 			logEvent = log.Info()
+		}
+		for key, value := range debugObjects {
+			logEvent.Any(key, value)
 		}
 		logEvent.Str("report_caller", caller).Int64("chat_id", chatID).Str("username", username).Msgf("Report: %s", text)
 	}
@@ -155,6 +170,17 @@ func (rc ReportConfig) Msg(text string) (*models.Message, error) {
 	})
 	if err != nil {
 		log.Error().Err(err).Str("text", msgText).Msg("Failed to send report message")
+	}
+	if err == nil && isTemp {
+		log.Trace().Msgf("The report message will be deleted after a minute...")
+		go func() {
+			time.Sleep(time.Minute)
+			rc.bot.DeleteMessage(context.Background(), &bot.DeleteMessageParams{
+				ChatID:    rc.recipientChatID,
+				MessageID: msg.ID,
+			})
+			log.Trace().Msgf("The report message is deleted.")
+		}()
 	}
 
 	return msg, err
