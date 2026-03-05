b0a5c8956b4635d0fce5d620202483cbbd4cf711
mazza <konstantinmaca@gmail.com>
2026-03-05 09:07:44 +0500

    fix(bot/admin): Fix formatting of `/chat` response message

--- internal/bots/admin/handlers.go
+++ internal/bots/admin/handlers.go
@@ -27,6 +27,12 @@ func (ab *AdminBot) registerHandlers() {
 	ab.bot.RegisterHandler(bot.HandlerTypeMessageText, "stats", bot.MatchTypeCommand, ab.statsHandler)
 	ab.bot.RegisterHandler(bot.HandlerTypeMessageText, "config", bot.MatchTypeCommand, ab.configHandler)
 	ab.bot.RegisterHandler(bot.HandlerTypeMessageText, "/dist_", bot.MatchTypePrefix, ab.distHandler)
+
+	ab.bot.RegisterHandlerMatchFunc(func(update *tgmodels.Update) bool {
+		_, err := models.ValidateGroupName(ab.services.Repo.DB, update.Message.Text)
+		return err == nil
+	}, ab.groupHandler)
+	// TODO: Handle username and chat_id.
 }
 
 func (ab *AdminBot) defaultHandler(ctx context.Context, b *bot.Bot, update *tgmodels.Update) {
@@ -37,13 +43,6 @@ func (ab *AdminBot) defaultHandler(ctx context.Context, b *bot.Bot, update *tgmo
 			// Remove not handled command.
 			bothelpers.DeleteMessageSafely(ctx, b, update.Message)
 		}
-
-		if _, err := models.ValidateGroupName(ab.services.Repo.DB, update.Message.Text); err == nil {
-			ab.groupHandler(ctx, b, update)
-			return
-		}
-
-		// TODO: Handle username and chat_id.
 	}
 }
 
@@ -88,28 +87,30 @@ Chat ID: <code>{{.TgChatID}}</code>
 Username: @{{.UserName}}
 State: {{.State}}
 Department: <code>{{.DepartmentName}}</code>
-Group: <code>{{.GroupNameEscaped}}</code>
-Daily Sending Time: {{.DailySendingTime}}
-Pair Sending: {{.PairSending}}
-Update Notification: {{.UpdateNotification}}
+Group: <code>{{.GroupName}}</code>
+Daily Sending Time: <code>{{.DailySendingTime}}</code>
+Pair Notificaton: {{.PairSending}}
+ChangeAlert: {{.ChangeAlert}}
 Access: {{.Access}}
 Recent Teachers: {{.RecentTeachers}}
 Recent updates:
-{{.RecentUpdates}}`
+<pre language="yaml">{{.RecentUpdates}}</pre>`
 
 var chatReportTemplate, _ = template.New("report").Parse(chatReportTemplateStr)
 
 func (ab *AdminBot) sendChatReport(chat *models.Chat, b *bot.Bot, ctx context.Context, update *tgmodels.Update) {
 	log.Trace().Msg("sendChatReport")
+
 	recentTeachers, err := models.GetTeacherByChatID(ab.services.Repo.DB, chat.ID)
 	if err != nil {
 		recentTeachers = []models.Teacher{}
 		ab.services.Reporter.Report().Err(err).Msgf("Failed to get recent teachers for chat %d", chat.ID)
 	}
-	recentTeachersNames := make([]string, len(recentTeachers))
+	recentTeacherNames := make([]string, len(recentTeachers))
 	for i, t := range recentTeachers {
-		recentTeachersNames[i] = t.Name
+		recentTeacherNames[i] = t.Name
 	}
+	recentTeachersStr := strings.Join(recentTeacherNames, ", ")
 
 	recentUpdates, err := models.GetRecentChatUpdateLogs(ab.services.Repo.DB, chat.ID, 48*time.Hour)
 	if err != nil {
@@ -118,22 +119,23 @@ func (ab *AdminBot) sendChatReport(chat *models.Chat, b *bot.Bot, ctx context.Co
 	}
 	var recentUpdatesStr strings.Builder
 	for i := len(recentUpdates) - 1; i >= 0 && i >= len(recentUpdates)-5; i-- {
-		recentUpdatesStr.WriteString(bot.EscapeMarkdown(fmt.Sprintf("- %s (time: %dms, error: %s)\n",
-			recentUpdates[i].Data, recentUpdates[i].HandlingTime, utils.DerefOrTypeDefault(recentUpdates[i].Error))))
+		fmt.Fprintf(&recentUpdatesStr, "- data: %v\n  time: %dms\n  error: %s\n",
+			recentUpdates[i].Data,
+			recentUpdates[i].HandlingTime,
+			utils.DerefOrTypeDefault(recentUpdates[i].Error),
+		)
 	}
 
 	log.Trace().Msg("Building report...")
 	var buf bytes.Buffer
 	chatReportTemplate.Execute(&buf, struct {
 		*models.Chat
-		GroupNameEscaped string
-		RecentTeachers   string
-		RecentUpdates    string
+		RecentTeachers string
+		RecentUpdates  string
 	}{
-		Chat:             chat,
-		GroupNameEscaped: bot.EscapeMarkdown(*chat.GroupName),
-		RecentTeachers:   bot.EscapeMarkdown(recentUpdatesStr.String()),
-		RecentUpdates:    bot.EscapeMarkdown(recentUpdatesStr.String()),
+		Chat:           chat,
+		RecentTeachers: recentTeachersStr,
+		RecentUpdates:  recentUpdatesStr.String(),
 	})
 	text := buf.String()
 
@@ -171,7 +173,7 @@ func (ab *AdminBot) groupHandler(ctx context.Context, b *bot.Bot, update *tgmode
 	}
 
 	var text strings.Builder
-	text.WriteString(bot.EscapeMarkdown(fmt.Sprintf("Chats in group `%s` (%d chats):\n", group, len(chats))))
+	fmt.Fprintf(&text, "Chats in group <code>%s</code> (%d chats):\n", group, len(chats))
 	for _, chat := range chats {
 		fmt.Fprintf(&text, "• <code>/chat %d</code>\n", chat.TgChatID)
 	}
